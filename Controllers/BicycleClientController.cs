06992fb08d229fd2b65b4071e0d5cea535d50d5d
ipsot <76401884+ipsot@users.noreply.github.com>
2022-08-10 14:24:58 +0300

    Add files via upload

---
+++ Controllers/BicycleClientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientBicycleWebApp.Controllers
+{
+    public class BicycleClientController : Controller
+    {
+        // GET: BicycleClient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
